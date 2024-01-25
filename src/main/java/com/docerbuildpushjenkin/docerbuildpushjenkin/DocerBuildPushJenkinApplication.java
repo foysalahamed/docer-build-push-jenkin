1e7664b02f8db062a16ee6620e5feae0bf1a92c0
Foysal Ahamed <foysal5@gmail.com>
2024-01-25 21:44:30 +0600

    first commit

---
+++ src/main/java/com/docerbuildpushjenkin/docerbuildpushjenkin/DocerBuildPushJenkinApplication.java
@@ -0,0 +1,13 @@
+package com.docerbuildpushjenkin.docerbuildpushjenkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocerBuildPushJenkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocerBuildPushJenkinApplication.class, args);
+	}
+
+}
